ed7c9ea8179eb223328133f39e1be651479a41aa
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-01-26 00:07:34 +0600

    -.-

--- project/my-app/src/routes/+page.svelte
+++ project/my-app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<!-- <script>
-  import { Carousel } from 'flowbite-svelte';
+<script>
+	// import 'workingcarousel.svelte';
+  // import { Carousel } from 'flowbite-svelte';
+  import Workingcarousel from './workingcarousel.svelte';
 
   const images = [
     {
@@ -23,42 +25,5 @@
 
 <div class="max-w-4xl">
   
-  <Carousel {images} Duration = {2000} />
-</div> -->
-
-<script>
-  import { Carousel } from 'flowbite-svelte';
-
-  const images = [
-    {
-      alt: 'Home1',
-      src: 'homepage1.webp',
-      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
-    },
-    {
-      alt: 'Home2',
-      src: 'homepage3.webp',
-      title: 'cristina-gottardi-CSpjU6hYo_0-unsplash.com'
-    },
-    {
-      alt: 'Johannes Plenio',
-      src: 'homepage2.jpg',
-      title: 'johannes-plenio-RwHv7LgeC7s-unsplash.com'
-    },
-  ];
-</script>
-hello
-hmph
-<div class="carousel-container min-h-screen">
-  <Carousel {images} Duration={2000} />
+  <Workingcarousel {images} Duration = {2000} />
 </div>
-
-<style>
-  .carousel-container {
-    height: 100vh; /* Make the container full-screen height */
-    width: 75vw/* Make the container full-screen width */;
-    overflow: hidden; /* Hide overflow to prevent scrollbar */
-  }
-
-  /* Additional styles for Carousel customization can be added here */
-</style>
