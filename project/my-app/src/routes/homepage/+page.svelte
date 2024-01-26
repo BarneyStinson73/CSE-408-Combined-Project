be2e2e81c8454e40d4714bc8bde7ed13ef83956b
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-01-27 02:09:20 +0600

    .......

--- project/my-app/src/routes/homepage/+page.svelte
+++ project/my-app/src/routes/homepage/+page.svelte
@@ -3,12 +3,64 @@
   import { ChartPieSolid, GridSolid, MailBoxSolid, UserSolid, BagSolid, ArrowRightToBracketSolid, FileEditSolid, FireSolid, BookSolid, WindowRestoreOutline, LifeBuoySolid } from 'flowbite-svelte-icons';
   import { Card, Dropdown, DropdownItem, Avatar, Button } from 'flowbite-svelte';
   import { DotsHorizontalOutline } from 'flowbite-svelte-icons';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
   let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Input } from 'flowbite-svelte';
+  import { SearchOutline } from 'flowbite-svelte-icons';
+
+import { Progressbar } from 'flowbite-svelte';
 </script>
 <!-- <img src="profile.jpg" alt="sample 1" class="w-64 h-64 rounded-full" /> -->
 
 
-<Card padding="md">
+<Navbar>
+  <NavBrand href="/">
+    <img src="logo-no-background.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white" style="color: #dede50;font-family: 'Montserrat Alternates', monospace">Victor</span>
+  </NavBrand>
+  <div class="flex md:order-2">
+    <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+      <SearchOutline class="w-5 h-5" />
+    </Button>
+    <div class="hidden relative md:block">
+      <div class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none">
+        <SearchOutline class="w-4 h-4" />
+      </div>
+      <Input id="search-navbar" class="ps-10" placeholder="Search..." />
+    </div>
+    <NavHamburger />
+  </div>
+  <NavUl>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/docs/components/navbar">Navbar</NavLi>
+  </NavUl>
+</Navbar>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+  .propic {
+    background-color: rgba(255, 255, 255, 0.5); /* Set the alpha value between 0 (completely transparent) and 1 (completely opaque) */
+    /* You can also set other styles as needed */
+  }
+</style>
+
+
+<Card padding="md" class="flex flex-col" color="rgb(255,255,255)">
   
   <div class="flex flex-col items-center pb-4">
     <img src="profile.jpg" class="w-64 h-64 rounded-full" alt="bonnie"/>
@@ -18,6 +70,7 @@
     </div>
   </div>
 </Card>
+
 
 
 <!-- 
@@ -67,4 +120,13 @@
 
 
 
+<Card>
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project 1</h5>
+  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Deadline:15 February 2024</p>
 
+  <Progressbar progress="50" size="h-4" labelInside style="background-color: lime"/>
+  <br>
+  <Button class="w-fit" style="background-color: green">
+    Details <ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+  </Button>
+</Card>
