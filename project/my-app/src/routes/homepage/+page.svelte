e53468be5b3b042dd7a3fb77e8a1cbd0bc988ae2
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-01-27 16:43:27 +0600

    git ignore added

--- project/my-app/src/routes/homepage/+page.svelte
+++ project/my-app/src/routes/homepage/+page.svelte
@@ -39,19 +39,6 @@ import { Progressbar } from 'flowbite-svelte';
 </Navbar>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 <style>
   .propic {
     background-color: rgba(255, 255, 255, 0.5); /* Set the alpha value between 0 (completely transparent) and 1 (completely opaque) */
@@ -70,51 +57,6 @@ import { Progressbar } from 'flowbite-svelte';
     </div>
   </div>
 <!-- </Card> -->
-
-
-
-<!-- 
-<Sidebar >
-  <SidebarWrapper>
-    <SidebarGroup>
-      <SidebarItem label="Dashboard">
-        <svelte:fragment slot="icon">
-          <ChartPieSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-      </SidebarItem>
-      <SidebarItem label="Projects" {spanClass}>
-        <svelte:fragment slot="icon">
-          <GridSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-      </SidebarItem>
-      <SidebarItem label="Notifications" {spanClass}>
-        <svelte:fragment slot="icon">
-          <MailBoxSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-        <svelte:fragment slot="subtext">
-          <span class="inline-flex justify-center items-center p-3 ms-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"> 3 </span>
-        </svelte:fragment>
-      </SidebarItem>
-      <SidebarItem label="Profile">
-        <svelte:fragment slot="icon">
-          <UserSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-      </SidebarItem>
-      <SidebarItem label="Sign Out">
-        <svelte:fragment slot="icon">
-          <ArrowRightToBracketSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-      </SidebarItem>
-    </SidebarGroup>
-    <SidebarGroup border>
-      <SidebarItem label="Help">
-        <svelte:fragment slot="icon">
-          <LifeBuoySolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-        </svelte:fragment>
-      </SidebarItem>
-    </SidebarGroup>
-  </SidebarWrapper>
-</Sidebar> -->
 
 
 
