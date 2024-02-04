72f62cbd57df755131d3fee85e908d41b0d7349e
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-02-04 21:38:23 +0600

    manager.js modified

--- project_backend/manager.js
+++ project_backend/manager.js
@@ -1,7 +1,56 @@
 const router = require("express").Router();
+const db = require("./db.js");
 
-router.route("/add").post((req, res) => {
-    
+router.route("/profile").post((req, res) => {
+    // const bearerHeader = req.headers["authorization"];
+    // if (!bearerHeader) {
+    //     return res.status(403).json({ message: "No token provided" });
+    // }
+    // const decoded = jwt.verify(bearerHeader, jwtSecret);
+
+    // let id = decoded["userId"];
+    // console.log(decoded);
+    let id = req.user["userId"];
+
+    db.any(`SELECT * FROM "User" WHERE "userId" = $1`, [id])
+        .then((data) => {
+            let response = {
+                message: "User profile retrieved successfully",
+                data: data,
+            };
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.route("/update_profile").post((req, res) => {
+    // const bearerHeader = req.headers["authorization"];
+    // if (!bearerHeader) {
+    //     return res.status(403).json({ message: "No token provided" });
+    // }
+    // const decoded = jwt.verify(bearerHeader, jwtSecret);
+
+    // let id = decoded["userId"];
+    // console.log(decoded);
+    let id = req.user["userId"];
+
+    let { username, email, password } = req.body;
+    db.any(
+        `UPDATE "User" SET "userName" = $1, "email" = $2, "password" = $3 WHERE "userId" = $4`,
+        [username, email, password, id]
+    )
+        .then((data) => {
+            let response = {
+                message: "User profile updated successfully",
+                data: data,
+            };
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
--- project_backend/projects.js
+++ project_backend/projects.js
@@ -1,23 +1,16 @@
 const router = require("express").Router();
-const pgp = require("pg-promise");
 const db = require("./db.js");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-// const router = express.Router();
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const jwtSecret = process.env.JWT;
 
 router.route("/").post((req, res) => {
-    const bearerHeader = req.headers["authorization"];
-    if (!bearerHeader) {
-        return res.status(403).json({ message: "No token provided" });
-    }
-    const decoded = jwt.verify(bearerHeader, jwtSecret);
+    // const bearerHeader = req.headers["authorization"];
+    // if (!bearerHeader) {
+    //     return res.status(403).json({ message: "No token provided" });
+    // }
+    // const decoded = jwt.verify(bearerHeader, jwtSecret);
 
-    let id = decoded["userId"];
-    console.log(decoded);
+    // let id = decoded["userId"];
+    // console.log(decoded);
+    let id=req.user["userId"];
     
     db.any(`SELECT * FROM "ProjectUser" WHERE "userId" = $1`, [id])
         .then((data) => {
