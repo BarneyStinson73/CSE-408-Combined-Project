540a9845b0644d24b7fd60a89eabb75412016f00
Tricky-Nabil <nabilsadiksakib17@gmail.com>
2024-02-11 12:59:46 +0600

    Gantt chart Updated

--- project/my-app/src/routes/gantt/GanttTable.svelte
+++ project/my-app/src/routes/gantt/GanttTable.svelte
@@ -1,9 +1,13 @@
 <script>
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
     import { onMount } from 'svelte';
     import Task from './Task.svelte'; // Define the Task component
   
     let project = {
+      index : '0',
       projectName: "Project X",
+      startDate : new Date('2024-02-10'),
+      deadline : new Date('2024-02-10'),
       tasks: [
         {
           taskName: "Task 1",
@@ -53,7 +57,9 @@
             index : str+childCount,
             taskName: task.taskName,
             startDate: task.startDate,
-            endDate: task.endDate
+            endDate: task.endDate,
+            startDateToLocal : task.startDate.toLocaleDateString(),
+            endDateToLocal : task.endDate.toLocaleDateString()
         }
         flatTasks.push(object);
         flatTasks.push(...flattenTasks(task.subtasks , str+childCount+'.'));
@@ -68,28 +74,39 @@
     onMount(() => {
       flatTasks = flattenTasks(project.tasks , '');
     });
+
+    console.log(flatTasks);
+    let tasks = [{index:1,name:"task 1"},{index : 2 , name:"task 2"}];
   </script>
+<div class = 'flex flex-row'>
+  <div class = 'basis-1/4'>
+<Table hoverable={true}>
+  <TableHead>
+    <TableHeadCell>Index</TableHeadCell>
+    <TableHeadCell>Task Name</TableHeadCell>
+    <TableHeadCell>Start Date</TableHeadCell>
+    <TableHeadCell>Deadline</TableHeadCell>
+  </TableHead>
+  <TableBody class="divide-y">
+    <TableBodyRow>
+      <TableBodyCell>{project.index}</TableBodyCell>
+      <TableBodyCell>{project.projectName}</TableBodyCell>
+      <TableBodyCell>{project.startDate.toLocaleDateString()}</TableBodyCell>
+      <TableBodyCell>{project.deadline.toLocaleDateString()}</TableBodyCell>
+    </TableBodyRow>
+    {#each flatTasks as task}
+    <TableBodyRow>
+      <TableBodyCell>{task.index}</TableBodyCell>
+      <TableBodyCell>{task.taskName}</TableBodyCell>
+      <TableBodyCell>{task.startDateToLocal}</TableBodyCell>
+      <TableBodyCell>{task.endDateToLocal}</TableBodyCell>
+    </TableBodyRow>
+    {/each}
+  </TableBody>
+</Table>
+</div>
+<div class='basis-1/2'> <h1>Gantt Chart</h1></div>
+
+</div>
   
-  <div class="overflow-x-auto">
-    <table class="table-auto min-w-full">
-      <thead>
-        <tr>
-          <th class="border px-4 py-2">Index</th>
-          <th class="border px-4 py-2">Task Name</th>
-          <th class="border px-4 py-2">Start Date</th>
-          <th class="border px-4 py-2">End Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each flatTasks as task}
-          <!-- <Task {task} /> -->
-          <tr>
-          <td>{task.index}</td>
-          <td>{task.taskName}</td>
-          <td>{task.startDate}</td>
-          <td>{task.endDate}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+  
