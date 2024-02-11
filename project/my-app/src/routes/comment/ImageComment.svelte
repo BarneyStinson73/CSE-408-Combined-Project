11c9678a0f75701adefed313569a995e645d71fe
Tricky-Nabil <nabilsadiksakib17@gmail.com>
2024-02-11 13:28:26 +0600

    Comment

---
+++ project/my-app/src/routes/comment/ImageComment.svelte
@@ -0,0 +1,9 @@
+<script>
+    let src = './comment.jpeg';
+    </script>
+    
+    <div class=" w-full">
+        <img src={src} alt="Your image description"
+          class="max-w-full max-h-full object-cover w-full h-full sticky top-0"
+        />
+    </div>
